7dc29a3ae437a4e1a6574ea9859e64f4e2e51bce
kmk_y_yuta <y.yamasaki@kmkworld.com>
2025-08-28 17:26:32 +0900

    tmp

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,5 +1,19 @@
 class ApiEndpoints {
   static const login = '/api/login';
-  static const reservations = '/api/reservations';
-  // 他のAPIエンドポイント
+  static const autoLogin = '/api/login';
+  static const getAccount = '/api/get_account';
+  static const getFamily = '/api/get_family';
+  static const getMyData = '/api/get_mydata';
+  static const updateMyData = '/api/update_mydata';
+  static const getClinic = '/api/get_clinic';
+  static const getAllClinic = '/api/get_all_clinic';
+  static const getShop = '/api/get_shop';
+  static const getEtcMenu = '/api/get_etc_menu';
+  static const getPoint = '/api/get_point';
+  static const getBadge = '/api/get_badge';
+  static const getNewsClearBadge = '/api/getnews_clearbadge';
+  static const getAnnounce = '/api/get_announce';
+  static const getNextReserve = '/api/get_next_reserve';
+  static const getFutureReserve = '/api/get_future_reserve';
+  static const reserveCancel = '/api/reserve_cancel';
 }
---
+++ lib/core/constants/setting.dart
@@ -0,0 +1,6 @@
+
+class AppSetting {
+  static const clinicId = 1;
+  static const branchId = 1;
+  static const registMailAddress = 'regist_ac_test@connect-app.net';
+}
