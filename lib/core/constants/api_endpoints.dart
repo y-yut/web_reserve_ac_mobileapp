f6346ab7a13d5759d875258c9ffcd44931d4e2b7
kmk_y_yuta <y.yamasaki@kmkworld.com>
2025-09-01 08:56:53 +0900

    tmp

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,19 +1,20 @@
 class ApiEndpoints {
-  static const login = '/api/login';
-  static const autoLogin = '/api/login';
-  static const getAccount = '/api/get_account';
-  static const getFamily = '/api/get_family';
-  static const getMyData = '/api/get_mydata';
-  static const updateMyData = '/api/update_mydata';
-  static const getClinic = '/api/get_clinic';
-  static const getAllClinic = '/api/get_all_clinic';
-  static const getShop = '/api/get_shop';
-  static const getEtcMenu = '/api/get_etc_menu';
-  static const getPoint = '/api/get_point';
-  static const getBadge = '/api/get_badge';
-  static const getNewsClearBadge = '/api/getnews_clearbadge';
-  static const getAnnounce = '/api/get_announce';
-  static const getNextReserve = '/api/get_next_reserve';
-  static const getFutureReserve = '/api/get_future_reserve';
-  static const reserveCancel = '/api/reserve_cancel';
+  static const host = 'https://web-reserve-ac.connect-app.net';
+  static const login = '$host/api/login';
+  static const autoLogin = '$host/api/login';
+  static const getAccount = '$host/api/get_account';
+  static const getFamily = '$host/api/get_family';
+  static const getMyData = '$host/api/get_mydata';
+  static const updateMyData = '$host/api/update_mydata';
+  static const getClinic = '$host/api/get_clinic';
+  static const getAllClinic = '$host/api/get_all_clinic';
+  static const getShop = '$host/api/get_shop';
+  static const getEtcMenu = '$host/api/get_etc_menu';
+  static const getPoint = '$host/api/get_point';
+  static const getBadge = '$host/api/get_badge';
+  static const getNewsClearBadge = '$host/api/getnews_clearbadge';
+  static const getAnnounce = '$host/api/get_announce';
+  static const getNextReserve = '$host/api/get_next_reserve';
+  static const getFutureReserve = '$host/api/get_future_reserve';
+  static const reserveCancel = '$host/api/reserve_cancel';
 }
